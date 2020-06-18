4f080bf68eb2e9cdb477a444eaf66189f1f167a4
Zulhamdiakbar <zulhamdi777@gmail.com>
2020-06-18 12:19:00 +0700

    Pertemuan9
    Zulhamdi Akbar - 152018128

---
+++ Pertemuan1/src/pertemuan1/Pertemuan1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author Zulhamdi Akbar
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        hitung();
+    }
+     public static void hitung() {
+        int hasil,panjang=8,lebar=8;
+        String nama="Zulhamdi Akbar";
+        String nrp="152018128";
+        hasil = panjang*lebar;
+        System.out.println(hasil);
+        System.out.println(nama);
+        System.out.println(nrp);
+    }
+    
+}
